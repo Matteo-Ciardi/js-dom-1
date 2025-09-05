317fed9f1ff273ef6782553c3c1f818501bcb2e6
MatteoCiardi <ciardimatteo99@gmail.com>
2025-09-05 10:21:41 +0200

    metodo alternativo - dopo la correzione

--- js/script.js
+++ js/script.js
@@ -1,25 +1,35 @@
 const myLampOff = document.querySelector(".off-lamp");
-const myLampOn = document.querySelector(".on-lamp");
+// const myLampOn = document.querySelector(".on-lamp");
 const myButton = document.querySelector(".my-button");
 
-let lampAccesa = false;
+// let lampAccesa = false;
 
 myButton.addEventListener('click', lampOn);
 
 
-function lampOn() {
-    if (lampAccesa) {
-        myLampOn.style.display = "block";
-        myLampOff.style.display = "none";
-        myButton.textContent = "SPEGNI";
-    }
+// function lampOn() {
+//     if (lampAccesa) {
+//         myLampOn.style.display = "block";
+//         myLampOff.style.display = "none";
+//         myButton.textContent = "SPEGNI";
+//     }
 
-    else {
-        myLampOn.style.display = "none";
-        myLampOff.style.display = "block";
+//     else {
+//         myLampOn.style.display = "none";
+//         myLampOff.style.display = "block";
+//         myButton.textContent = "ACCENDI";
+//     }
+
+//     lampAccesa = !lampAccesa;
+// }
+
+function lampOn() {
+    if (myLampOff.src.includes("white_lamp")) {
+        myLampOff.src = "img/yellow_lamp.png";
+        myButton.textContent = "SPEGNI";
+    } else {
+        myLampOff.src = "img/white_lamp.png";
         myButton.textContent = "ACCENDI";
     }
 
-    lampAccesa = !lampAccesa;
 }
-
